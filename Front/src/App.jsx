a03005db8ac120eb3f1e03166623ebee6e3d670c
Juarez-Diego <diegoalbertojuarez@yahoo.com>
2023-04-03 21:31:29 -0600

    Added react-router-dom

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,28 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from './Redux/counter-test'
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import './App.css'
+import Home from './Components/Home'
 
 function App() {
-  const count = useSelector((state) => state.dog.testValue)
-  const dispatch = useDispatch()
 
   return (
     <div className="App">
-       <div className='bg-nuestros-perfiles'>
-       <h1 className="text-2xl font-['Geogrotesque'] text-primary">
-      Hello world!
-    </h1>
-
-{/* Aqui esta un ejemplo de un Counter usando Redux Toolkit */}
-    <div>
-      <h1 className="text-2xl font-['Geogrotesque'] text-primary">{count} minutes</h1>
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
-    </div>
-       </div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ Front/src/Components/Home.jsx
@@ -0,0 +1,29 @@
+
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../Redux/counter-test'
+import React from 'react'
+
+function Home() {
+    const count = useSelector((state) => state.dog.testValue)
+    const dispatch = useDispatch()
+  
+    return (
+      <div className="App">
+         <div className='bg-nuestros-perfiles'>
+         <h1 className="text-2xl font-['Geogrotesque'] text-primary">
+        Hello world!
+      </h1>
+  
+  {/* Aqui esta un ejemplo de un Counter usando Redux Toolkit */}
+      <div>
+        <h1 className="text-2xl font-['Geogrotesque'] text-primary">{count} minutes</h1>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+      </div>
+         </div>
+      </div>
+    )
+  }
+  
+  export default Home;
+  
