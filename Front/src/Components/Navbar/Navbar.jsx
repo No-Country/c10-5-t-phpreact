369e0db7beaf29606959ee563d26155e46b08811
Juarez-Diego <diegoalbertojuarez@yahoo.com>
2023-04-03 23:33:01 -0600

    Almost finished Navbar

---
+++ Front/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+    return (
+        <div>
+            <nav className="flex bg-primary h-[112px] items-center">
+                <ul className="flex flex-row justify-between items-center w-full px-[15px]">
+                    <div className="flex flex-row items-center">
+                        <img src="../../src/assets/Puzzle_NC.png" alt="NC_Logo" className="px-[10px]"/>
+                        <li className="font-Geogrotesque text-[60px] font-[600] text-white px-[10px] tracking-[-0.03em]">No Country</li>
+                    </div>
+
+                    <div className="flex flex-row justify-around items-center border-black w-[600px] font-Roboto text-[20px] font-[500] text-text-color-white">
+                        <li>Roles</li>
+
+                        <li>Verticales</li>
+
+                        <li>Preguntas Frecuentes</li>
+
+                        <button className="bg-[#5173EB] w-[161px] h-[56px] rounded-[12px] shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">Plataforma</button>
+                    </div>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Navbar from './Components/Navbar/Navbar'
 import { store } from './Redux/store'
 import { Provider } from 'react-redux'
 import './index.css'
@@ -8,6 +9,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <React.StrictMode>
+      <Navbar />
       <App />
     </React.StrictMode>
   </Provider>
