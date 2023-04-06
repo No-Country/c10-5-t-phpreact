dc995b977823cae6f498db2e965b0cf11c0d1d3e
Juarez-Diego <diegoalbertojuarez@yahoo.com>
2023-04-06 02:36:52 -0600

    section 2 90% finished

--- Front/src/Components/Home/Home.jsx
+++ Front/src/Components/Home/Home.jsx
@@ -4,6 +4,7 @@ import { decrement, increment } from '../../Redux/counter-test'
 import React from 'react'
 import Footer from '../Footer/Footer'
 import Section_1 from './Section_1'
+import Section_2 from './Section_2'
 
 function Home() {
     const count = useSelector((state) => state.dog.testValue)
@@ -12,6 +13,7 @@ function Home() {
     return (
       <div className="App">
         <Section_1 />
+        <Section_2 />
         <Footer />
       </div>
     )
