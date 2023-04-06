306b5635d115d8e096744eaa0a845978e8eb7a42
Juarez-Diego <diegoalbertojuarez@yahoo.com>
2023-04-06 03:10:37 -0600

    finished section 3 90%

--- Front/src/Components/Home/Home.jsx
+++ Front/src/Components/Home/Home.jsx
@@ -5,6 +5,7 @@ import React from 'react'
 import Footer from '../Footer/Footer'
 import Section_1 from './Section_1'
 import Section_2 from './Section_2'
+import Section_3 from './Section_3'
 
 function Home() {
     const count = useSelector((state) => state.dog.testValue)
@@ -14,6 +15,7 @@ function Home() {
       <div className="App">
         <Section_1 />
         <Section_2 />
+        <Section_3 />
         <Footer />
       </div>
     )
