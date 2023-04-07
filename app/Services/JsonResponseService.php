c9e4c9276498c147faf2e43bc056d909bd524374
Alan Sanchez <alaansannchezz@gmail.com>
2023-04-07 00:18:50 -0300

    Se realizaron pequeños cambios

--- app/Services/JsonResponseService.php
+++ app/Services/JsonResponseService.php
@@ -7,11 +7,14 @@ use Symfony\Component\HttpFoundation\Response;
 
 class JsonResponseService
 {
-    public function success(string $action): JsonResponse
+    public function success(string $action, string $key = null, mixed $value = null): JsonResponse
     {
-        return response()->json([
-            'msg' => "Se ha {$action} satisfactoriamente."
-        ], Response::HTTP_OK);
+        $data = [
+            'msg' => "Se ha {$action} satisfactoriamente.",
+            ($key === null && $value === null) ?: $key => $value,
+        ];
+
+        return response()->json(array_filter($data), Response::HTTP_OK);
     }
 
     public function catch(string $error): JsonResponse
@@ -19,5 +22,12 @@ class JsonResponseService
         return response()->json([
             'error' => $error
         ], Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+
+    public function missingImage(): JsonResponse
+    {
+        return response()->json([
+            'error' => 'Falta una imagen.'
+        ], Response::HTTP_BAD_REQUEST);
     }
 }
