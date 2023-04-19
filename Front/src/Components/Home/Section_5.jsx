46ca1e78d0138d3bf1e7b2bdaacfb228498802fc
Juarez-Diego <diegoalbertojuarez@yahoo.com>
2023-04-19 05:48:11 -0600

    fixed boy image, working on faqs

--- Front/src/Components/Home/Section_5.jsx
+++ Front/src/Components/Home/Section_5.jsx
@@ -1,53 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import {faq, accordion} from "./Questions";
+
 
 function Section_5(){
+
+    const [active, setActive] = useState(0)
+
+    function toggle(){
+       if(active === 0){
+        setActive(1)
+       } else{
+        setActive(0)
+       }
+    }
+
+    // console.log(active)
     return(
-        <div id="section_5" className="flex w-full justify-evenly px-[20px] pt-[30px] items-center ">
+        <div id="section_5" className="flex w-full h-[500px] justify-evenly px-[20px] pt-[30px] items-center relative border-2 border-black">
             
 
-            <div className="flex flex-col justify-around h-[450px]">
+            <div className="flex flex-col justify-around h-full border-2 border-black mr-[400px]">
                 <div className="flex bg-primary w-[766px] h-[56px] rounded-[10px] shadow-dark items-center">
                     <span className="font-Inter text-[20px] font-[500] text-text-color-white tracking-[-0.03em] leading-[45px] indent-4">Preguntas Frecuentes</span>
                 </div>
 
-                <button className="flex justify-between bg-secondary w-[609px] h-[56px] rounded-[10px] shadow-dark items-center">
-                    <span htmlFor="pregunta_1" className="ont-Inter text-[16px] font-[500] tracking-[-0.03em] leading-[56px] indent-4">
-                        ¿Ademas de desarrolladores, se pueden inscribir otros perfiles?
-                    </span>
-                    <img src="../../src/assets/vector.png" alt="vector"  className="mr-[15px]"/>
-                </button>
-                
-                <button className="flex justify-between bg-secondary w-[609px] h-[56px] rounded-[10px] shadow-dark items-center">
-                    <span htmlFor="pregunta_1" className="ont-Inter text-[16px] font-[500] tracking-[-0.03em] leading-[56px] indent-4">
-                        ¿Puedo inscribirme como desarrollador si solo se HTML y CSS?
-                    </span>
-                    <img src="../../src/assets/vector.png" alt="vector"  className="mr-[15px]"/>
-                </button>
-
-                <button className="flex justify-between bg-secondary w-[609px] h-[56px] rounded-[10px] shadow-dark items-center">
-                    <span htmlFor="pregunta_1" className="ont-Inter text-[16px] font-[500] tracking-[-0.03em] leading-[56px] indent-4">
-                        No tengo experiencia mínima ¿Que puedo hacer?
-                    </span>
-                    <img src="../../src/assets/vector.png" alt="vector"  className="mr-[15px]"/>
-                </button>
-
-                <button className="flex justify-between bg-secondary w-[609px] h-[56px] rounded-[10px] shadow-dark items-center">
-                    <span htmlFor="pregunta_1" className="ont-Inter text-[16px] font-[500] tracking-[-0.03em] leading-[56px] indent-4">
-                        ¿Cual es el cronograma de emulación?
-                    </span>
-                    <img src="../../src/assets/vector.png" alt="vector"  className="mr-[15px]"/>
-                </button>
-
-                <button className="flex justify-between bg-secondary w-[609px] h-[56px] rounded-[10px] shadow-dark items-center">
-                    <span htmlFor="pregunta_1" className="ont-Inter text-[16px] font-[500] tracking-[-0.03em] leading-[56px] indent-4">
-                        ¿Puedo participar en mas de una emulación?
-                    </span>
-                    <img src="../../src/assets/vector.png" alt="vector"  className="mr-[15px]"/>
-                </button>
+                {faq.map(e => (
+                      <div className="w-[609px] h-[56px] bg-secondary rounded-[10px] shadow-dark items-center group is-active cursor-pointer">
+                      <div className="flex items-center">
+                          <div className="w-full font-Inter text-[16px] font-[500] tracking-[-0.03em] leading-[56px] indent-4">{e.question}</div>
+                          <div className="font-Inter text-[20px] font-[500] rotate-90 mr-[15px]">&gt;</div>
+                          {/* <img src="../../src/assets/vector.png" alt="vector" className="mr-[15px] group-[.is-active]:rotate-[180deg]" /> */}
+                      </div>
+          
+                      <div className='overflow-hidden max-h-0 group-[is.active]:max-h-auto'>
+                          {e.answer}
+                      </div>
+                  </div>
+                ))}
                 
             </div>
 
-            <div className="mt-[100px]">
+            <div className="absolute bottom-0 right-20">
                 <img src="../../src/assets/selfie_boy.png" alt="boy"/>
             </div>
         </div>
